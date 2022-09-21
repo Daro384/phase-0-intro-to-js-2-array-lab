93bbf8f95c9ff3d232c8fff8f2d1bd69e212ad16
Joseph <joffan98@gmail.com>
2022-09-21 18:54:05 -0500

    commit tree dfff8814eb505976dd13a02d361ddced3748e174

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat = kitten => {
+    cats.push(kitten)
+}
+
+const destructivelyPrependCat = kitten => {
+    cats.unshift(kitten)
+}
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop()
+}
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift()
+}
+
+const appendCat = kitten => {
+    const newCats = [...cats]
+    newCats.push(kitten)
+    return newCats
+}
+
+const prependCat = kitten => {
+    const newCats = [...cats]
+    newCats.unshift(kitten)
+    return newCats
+}
+
+const removeLastCat = () => {
+    const newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+
+const removeFirstCat = () => {
+    const newCats = [...cats]
+    newCats.shift()
+    return newCats
+}
